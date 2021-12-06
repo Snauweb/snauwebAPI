7517122cdf5ad4867a142acd0183d4ffc268554d
Grimmbol <krogstie.oystein@gmail.com>
2021-12-06 18:48:25 +0100

    add bruker/alias endpoint that lists current active user aliases

--- api.py
+++ api.py
@@ -26,6 +26,38 @@ bugge.read_config(configPath)
 bugge.init_DB()
 
 
+# Shows all active aliases
+@bugge.route("/bruker/alias", "GET")
+def get_active_aliases():
+    cursor = bugge.get_DB_cursor()
+
+    query = \
+        'SELECT username, brukeralias FROM \
+        (SELECT userid, username from "tbl_User") AS u \
+        INNER JOIN brukeralias AS b ON u.userid = b.brukerid \
+        '
+
+    cursor.execute(query);
+    
+    row_count = 0
+    rows = []
+    for row in cursor:
+        row_count += 1
+        rows += [row]
+
+    response = [{} for x in range(0, row_count)]
+    row_count = 0
+    for row in rows:
+        response[row_count] = {
+            "brukernavn": row[0],
+            "brukeralias": row[1],
+        }
+        row_count += 1
+        
+    bugge.respond_JSON(response) 
+    
+    cursor.close()
+
 # As of now, any authenticated user is allowed to view the same user info
 # Accepts id as a query parameter. If more ids, pick first
 @bugge.route("/bruker", "GET")
