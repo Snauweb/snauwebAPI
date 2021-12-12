535f918024659eb2e3121cf2feb1a13b94f9aad4
Karonline98 <82039121+Karonline98@users.noreply.github.com>
2021-12-12 19:51:16 +0100

    Update forslag.sql

--- schemas/forslag.sql
+++ schemas/forslag.sql
@@ -5,5 +5,15 @@ CREATE TABLE forslag (
        tittel VARCHAR(256) NOT NULL,
        forslag TEXT NOT NULL,
        lagt_til TIMESTAMP WITHOUT TIME ZONE,
-       brukerid INTEGER REFERENCES "tbl_User"(userid)		
+       brukerid INTEGER REFERENCES "tbl_User"(userid),
+       statusid INTEGER REFERENCES "forslagstatusr"(statusid)
+       
 );
+
+
+CREATE TABLE forslagstatus (
+       statusid SERIAL PRIMARY KEY,
+       beskrivelse VARCHAR(30) NOT NULL
+);
+
+
